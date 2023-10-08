8db51293f9028b0a17ec7f6cca21a89ff9b6cee0
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-08 09:26:02 -0500

    Finished front end globe rendering

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
+import 'cesium/Build/Cesium/Cesium'
 
 class CesiumMap extends Component {
   componentDidMount() {
@@ -12,14 +13,14 @@ class CesiumMap extends Component {
       terrain: Terrain.fromWorldTerrain(),
     });
 
-    // Fly the camera to San Francisco at the given longitude, latitude, and height
-    viewer.camera.flyTo({
-      destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-      orientation: {
-        heading: CesiumMath.toRadians(0.0),
-        pitch: CesiumMath.toRadians(-15.0),
-      },
-    });
+    // // Fly the camera to San Francisco at the given longitude, latitude, and height
+    // viewer.camera.flyTo({
+    //   destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    //   orientation: {
+    //     heading: CesiumMath.toRadians(0.0),
+    //     pitch: CesiumMath.toRadians(-15.0),
+    //   },
+    // });
 
     // Add Cesium OSM Buildings, a global 3D buildings layer
     createOsmBuildingsAsync().then((buildingTileset) => {
