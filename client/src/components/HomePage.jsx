dc881eb9fabab6e6fc5f8177cbc1e3b4d435e70d
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-08 09:02:10 -0500

    Got token working with cesium with SF as a demo

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import {Viewer} from "cesium"
+import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 class CesiumMap extends Component {
   componentDidMount() {
-    
-    // Initialize Cesium viewer in the component's DOM element
-    const viewer = new Viewer(this.cesiumContainer);
+    // Cesium Ion access token
+    Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmNTUzNTA4Yy01YTNiLTQ1ZDYtOTAzNi1jZjY0NTRlNjQyNTYiLCJpZCI6MTcwNjUxLCJpYXQiOjE2OTY3NzE4OTF9.H6QbhLHrhp_Culob6xtjd56owzBtTWgSDNoZHOWe7Fs';
+
+    // Initialize the Cesium Viewer in the component's DOM element
+    const viewer = new Viewer(this.cesiumContainer, {
+      terrain: Terrain.fromWorldTerrain(),
+    });
+
+    // Fly the camera to San Francisco at the given longitude, latitude, and height
+    viewer.camera.flyTo({
+      destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+      orientation: {
+        heading: CesiumMath.toRadians(0.0),
+        pitch: CesiumMath.toRadians(-15.0),
+      },
+    });
+
+    // Add Cesium OSM Buildings, a global 3D buildings layer
+    createOsmBuildingsAsync().then((buildingTileset) => {
+      viewer.scene.primitives.add(buildingTileset);
+    });
   }
 
   render() {
-    return <div ref={(element) => (this.cesiumContainer = element)} />;
+    return <div ref={(element) => (this.cesiumContainer = element)} style={{ height: '100vh' }} />;
   }
 }
 
