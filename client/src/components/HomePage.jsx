2c9bd7b73206313143fbcacb9aa1b0d5fe56d777
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-08 08:56:01 -0500

    front end serving, still needs custom token.

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,9 @@
-import { React, Component } from 'react';
-import Viewer from "cesium"
-
-const cesiumDefaultToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmNTUzNTA4Yy01YTNiLTQ1ZDYtOTAzNi1jZjY0NTRlNjQyNTYiLCJpZCI6MTcwNjUxLCJpYXQiOjE2OTY3NzE4OTF9.H6QbhLHrhp_Culob6xtjd56owzBtTWgSDNoZHOWe7Fs"
+import React, { Component } from 'react';
+import {Viewer} from "cesium"
 
 class CesiumMap extends Component {
   componentDidMount() {
-    Cesium.Ion.defaultAccessToken = cesiumDefaultToken
+    
     // Initialize Cesium viewer in the component's DOM element
     const viewer = new Viewer(this.cesiumContainer);
   }
