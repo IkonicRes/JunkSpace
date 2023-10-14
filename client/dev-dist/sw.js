e4a611eb6e36975057ea95900a255151438824c6
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-13 19:20:06 -0500

    newest push, rebuilt

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-296dd616'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3av2edgo00g"
+    "revision": "0.bmv6io6hpvo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
