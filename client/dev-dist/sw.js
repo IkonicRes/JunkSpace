89046bce066e36134994ae738272d365d337082f
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-10-14 09:59:43 -0500

    pull updated code

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-296dd616'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0q84ng8tav"
+    "revision": "0.r3o6ju4sr7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
