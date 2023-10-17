d866754b616b06d7f1a578982c7a91eac690695e
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-10-17 18:51:37 -0500

    fixed errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import cesium from 'vite-plugin-cesium';
 
 import { VitePWA } from 'vite-plugin-pwa';
 // import { injectManifest } from 'workbox-build';
