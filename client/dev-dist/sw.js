98f51ef3b6c5162bb073f4bca6cf6a1f8ea34dd0
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-19 17:02:12 -0500

    2nd compilation

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-296dd616'], (function (workbox) { 'use strict';
+define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-296dd616'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r51g0lbflh"
+    "revision": "0.3ees014evio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/src/utils/cartContext.jsx
+++ client/src/utils/cartContext.jsx
@@ -1,23 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 
-export const CartContext = createContext();
+const CartContext = createContext();
 
-export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
 
-  const addToCart = (product) => {
-    // add product to cart
-  };
-
-  const removeFromCart = (productId) => {
-    // remove product from cart
-  };
-
-  return (
-    <CartContext.Provider 
-      value={{ cart, addToCart, removeFromCart }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
-}
+export { CartContext }
