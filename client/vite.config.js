967d5a2e40d810aa65a4dd030f88885c3b99ee8d
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-19 18:03:19 -0500

    fixed front end

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,32 +2,30 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium'
-import Graphql from '@rollup/plugin-graphql'
 // import { injectManifest } from 'workbox-build';
 
 export default defineConfig({
   optimizeDeps: {
-    include: ['satellite.js', 'axios'], // Include satellite.js in the optimized dependencies
+    include: ['cesium', 'satellite.js', 'axios'], // Include satellite.js in the optimized dependencies
   },
   plugins: [
     cesium(),
     react(),
-    Graphql(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: {
-          enabled: true
-        },
-        injectRegister: 'auto'
-      }),
+    
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto'
+    }),
   ],
   server: {
     port: 4000,
     open: true,
     proxy: {
       '/graphql': {
-        // target: '/.netlify/functions/graphql', // Replace with your GraphQL server URL
-        target: "/graphql",
+        target: 'http://localhost:4001',
         changeOrigin: true,
         secure: false,
       },
