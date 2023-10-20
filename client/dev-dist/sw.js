767e39d13b3c2c8b748c53640764d9f32b158981
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-19 22:03:51 -0500

    fix3

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.82d2m6hj7r"
+    "revision": "0.sac39cqlem8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
