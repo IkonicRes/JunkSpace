b2401884898ea1d106c3f5eed28d88d2884d8293
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-10-20 09:22:16 -0500

    Set cors url 2

--- server/server.js
+++ server/server.js
@@ -34,8 +34,10 @@ const server = new ApolloServer({
 });
 
 var corsOptions = {
-  origin: 'https://junkspace.onrender.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  "origin": "https://junkspace-zcpt.onrender.com/*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
 }
 
 const app = express();
